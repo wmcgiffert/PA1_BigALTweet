f8bc6eb0915f2b40efa25db4722a0cbeef283d65
wmcgiffert <wmcgiffert@crimson.ua.edu>
2020-09-02 20:10:16 -0500

    V2

--- Post.cs
+++ Post.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace PA_1
 {
-    public class Post
+    public class Post : IComparable<Post>
     {
         public Guid UserID {get; set;}
         public string PostMessage {get; set;}
@@ -12,6 +12,10 @@ namespace PA_1
         public override string ToString()
         {
             return this.UserID + "#" + this.PostMessage + "#" + Time;
-        } 
+        }
+        public int CompareTo(Post temp)
+        {
+            return this.UserID.CompareTo(temp.UserID);
+        }
     }
 }
